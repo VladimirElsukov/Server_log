fe85b070aa1a4c1c1fc307ffbd9d4115c2471b35
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-05-18 11:19:50 +0300

    Добавляем сортировку ip-адреса по убыванию частоты и по числовому значению

--- server_log_frequency2.py
+++ server_log_frequency2.py
@@ -19,8 +19,9 @@ for ip in ip_addresses:
     ip_frequency[ip] += 1
 
 try:
-    # Сортировка IP-адресов по убыванию частоты упоминаний и IP-адреса по убыванию
-    sorted_ips = sorted(ip_frequency.items(), key=lambda x: (-x[1], x[0]))
+    # Сконвертируем IP-адреса в числовой формат для правильной сортировки
+    sorted_ips = sorted(ip_frequency.items(), key=lambda x: (-x[1], list(map(int, x[0].split('.'))))
+
     # Запись результатов в новый файл
     with open('ip_frequency.txt', 'w') as output_file:
         for ip, count in sorted_ips:
