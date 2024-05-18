3393523f68ff010af77b3761a65550e7e69b5484
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-05-18 10:49:22 +0300

    Вносим изменения в код для получения пути к текущей дирректории скрипта

--- server_log_frequency2.py
+++ server_log_frequency2.py
@@ -1,8 +1,15 @@
 import re
+import os
 from collections import defaultdict
 
+# Получаем путь к текущей директории скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем абсолютный путь к файлу 'log.txt' в той же директории, что и скрипт
+file_path = os.path.join(current_dir, 'log.txt')
+
 ip_addresses = []
-with open('log.txt', 'r') as file:
+with open(file_path, 'r') as file:
     for line in file:
         ips = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line)
         ip_addresses.extend(ips)
