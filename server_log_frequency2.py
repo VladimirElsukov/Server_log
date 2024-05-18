ccf7cab4785058266c479afe20e7e95661bb10f3
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-05-18 10:25:03 +0300

    Добавили обработку ощибок при сортировке или записи

--- server_log_frequency2.py
+++ server_log_frequency2.py
@@ -11,11 +11,12 @@ ip_frequency = defaultdict(int)
 for ip in ip_addresses:
     ip_frequency[ip] += 1
 
-
-# Сортировка IP-адресов по убыванию частоты упоминаний и IP-адреса по убыванию
-sorted_ips = sorted(ip_frequency.items(), key=lambda x: (-x[1], x[0]))
-
-# Запись результатов в новый файл
-with open('ip_frequency.txt', 'w') as output_file:
-    for ip, count in sorted_ips:
-        output_file.write(f"{ip} {count}\n")
+try:
+    # Сортировка IP-адресов по убыванию частоты упоминаний и IP-адреса по убыванию
+    sorted_ips = sorted(ip_frequency.items(), key=lambda x: (-x[1], x[0]))
+    # Запись результатов в новый файл
+    with open('ip_frequency.txt', 'w') as output_file:
+        for ip, count in sorted_ips:
+            output_file.write(f"{ip} {count}\n")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
